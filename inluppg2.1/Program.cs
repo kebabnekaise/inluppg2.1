88e02ed70b7c73edb3e2e3643c4be554629c01cb
emsi06001 <emsi06001@E-PW0234N3>
2023-09-13 12:57:20 +0200

    Add project files.

---
+++ inluppg2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace inluppg2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favoritbok?");
+            string namn = Console.ReadLine();
+            Console.WriteLine($"{namn}");
+        }
+    }
+}
